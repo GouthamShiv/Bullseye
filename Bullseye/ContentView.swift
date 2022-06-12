a6a9ee733260f1720be4b9f1e7bab02355eeb8da
goutham <gouthamshine@gmail.com>
2022-06-12 19:41:15 +0530

    created the initial, non interactive view for the app

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+
+            Text("89")
+                .kerning(-1.0)
+                .fontWeight(.black)
+                .font(.largeTitle)
+            
+            HStack {
+                Text("1")
+                    .bold()
+                
+                Slider(value: .constant(50), in: 1.0...100.0)
+                
+                Text("100")
+                    .bold()
+            }
+            
+            Button(action: {}) {
+                Text("HIT ME")
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
