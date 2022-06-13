85617a54362536e20aebdceef00d092d3ede0d91
goutham <gouthamshine@gmail.com>
2022-06-13 11:27:21 +0530

    added assets for customising the elements and some UI updates

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,55 +14,63 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .padding(.bottom)
-            
-            Text("Put the bullseye as close as you can to".uppercased())
-                .bold()
-                .kerning(2)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-                .padding(.bottom)
-
-            Text(String(game.target))
-                .kerning(-1.0)
-                .fontWeight(.black)
-                .font(.largeTitle)
-            
-            HStack {
-                Text("1")
-                    .bold()
+        ZStack {
+            Color("BackgroundColor").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("🎯🎯🎯")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .padding(.bottom)
                 
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                
-                Text("100")
+                Text("Put the bullseye as close as you can to".uppercased())
                     .bold()
-            }
-            .padding()
-            
-            Button(action: {
-                isAlertVisible = true
-            }) {
-                Text("hit me".uppercased())
-                .bold()
-                .font(.title3)
-            }
-            .padding(20.0)
-            .background(Color.blue)
-            .foregroundColor(Color.white)
-            .cornerRadius(21)
-            .alert("Your Target: \(game.target)!", isPresented: $isAlertVisible) {
-                Button("Awesome") {}
-            } message: {
-                let roundedInt = Int(sliderValue.rounded())
-                Text("The slider's value is \(roundedInt)!\n" +
-                     "You scored \(game.points(sliderValue: roundedInt)) points this round.")
-            }
+                    .kerning(2)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .padding(.bottom)
 
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                
+                HStack {
+                    Text("1")
+                        .bold()
+                    
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    
+                    Text("100")
+                        .bold()
+                }
+                .padding()
+                
+                Button(action: {
+                    isAlertVisible = true
+                }) {
+                    Text("hit me".uppercased())
+                    .bold()
+                    .font(.title3)
+                }
+                .padding(20.0)
+                .background(ZStack {
+                    Color("ButtonColor")
+                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                })
+                .foregroundColor(Color.white)
+                .cornerRadius(21)
+                .alert(
+                    "Your Target: \(game.target)!",
+                    isPresented: $isAlertVisible,
+                    actions: { Button("Awesome") {} },
+                    message: {
+                        let roundedInt = Int(sliderValue.rounded())
+                        Text("The slider's value is \(roundedInt)!\n" +
+                             "You scored \(game.points(sliderValue: roundedInt)) points this round.")
+                    }
+                )
+            }
         }
     }
 }
