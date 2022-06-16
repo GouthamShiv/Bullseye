11dcd5e5f54e74760b98f5914487f67269118e32
Goutham S <gouthamshine@gmail.com>
2022-06-16 14:12:49 +0530

    removed commented code

--- Bullseye/Views/ControlsView.swift
+++ Bullseye/Views/ControlsView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct ButtonView: View {
     @Binding var isAlertVisible: Bool
-//    @Binding var game: Game
-//    @Binding var sliderValue: Double
 
     var body: some View {
         Button(action: {
@@ -33,27 +31,11 @@ struct ButtonView: View {
             RoundedRectangle(cornerRadius: 21)
                 .strokeBorder(Color.white, lineWidth: 2.0)
         )
-//        .alert(
-//            "Your Target: \(game.target)!",
-//            isPresented: $isAlertVisible,
-//            actions: {
-//                Button("Awesome!") {
-//                    game.startNewRound(points: game.points(sliderValue: Int(sliderValue.rounded())))
-//                }
-//            },
-//            message: {
-//                let roundedValue = Int(sliderValue.rounded())
-//                let points = game.points(sliderValue: roundedValue)
-//                Text("You've hit \(roundedValue)!\n" +
-//                     "You scored \(points) points this round.")
-//            }
-//        )
     }
 }
 
 struct ButtonViewPreviews: PreviewProvider {
     static var previews: some View {
-//        ButtonView(isAlertVisible: .constant(true), game: .constant(Game()), sliderValue: .constant(50.0))
         
         ButtonView(isAlertVisible: .constant(false))
         
